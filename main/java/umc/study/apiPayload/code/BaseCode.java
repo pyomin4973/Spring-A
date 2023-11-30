05347daa9d2ce418d53aec10d74b19cd57a01616
pyomin4973 <72841908+pyomin4973@users.noreply.github.com>
2023-11-30 20:23:22 +0900

    8(2)주차 미션

---
+++ main/java/umc/study/apiPayload/code/BaseCode.java
@@ -0,0 +1,10 @@
+package umc.study.apiPayload.code;
+
+import java.awt.desktop.UserSessionEvent;
+
+public interface BaseCode {
+
+    public ReasonDTO getReason();
+
+    public ReasonDTO getReasonHttpStatus();
+}
---
+++ main/java/umc/study/domain/enums/MemberStatus.java
@@ -0,0 +1,6 @@
+package umc.study.domain.enums;
+
+public enum MemberStatus {
+    ACTIVE, INACTIVE
+}
+
